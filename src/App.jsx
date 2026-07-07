e3e752b39541b583b01ce345bda60fe33ac7cbc9
baqirraza5 <sbaqarskp786@gmail.com>
2026-07-07 16:38:20 +0100

    feat: add lesson 07 - rebuilding with react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import JobCard from "./components/JobCard";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="cardList">
+      <JobCard />
+      <JobCard />
+      <JobCard />
+    </div>
+  );
+}
+export default App;
---
+++ src/components/JobCard.jsx
@@ -0,0 +1,14 @@
+const JobCard = () => {
+  return (
+    <div className="card">
+      <div className="logo" />
+      <div className="info">
+        <h3>AI Product Engineer</h3>
+        <p>Google - £55000</p>
+        <p>London</p>
+      </div>
+    </div>
+  );
+};
+
+export default JobCard;
